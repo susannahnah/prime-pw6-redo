4d5b93dc12f5a46882d6eaf25fd8026b1a95a3d4
Alyssa Boyd <ally@primeacademy.io>
2018-03-23 11:10:33 -0500

    Add in client solution

--- client.js
+++ client.js
@@ -11,5 +11,20 @@ let garage = [];
 function newCar( year, make, model ){
   console.log( 'in newCar:', year, make, model );
   garage.push( new Car( year, make, model ) );
+  displayCars();
   return true;
 } // end newCar
+
+function displayCars(){
+  let $el = $( '#carsOut' );
+  $el.empty();
+  for( car of garage ){
+    $el.append( '<li>' + car.year + ' ' + car.make + ' ' + car.model + '</li>' );
+  } //end for
+} // end displayCars
+
+$( document ).ready( function(){
+  $( '#addCarButton' ).on( 'click', function(){
+    newCar( $( '#yearIn' ).val(), $( '#makeIn' ).val(), $( '#modelIn' ).val() );
+  }); //end addCarButton on click
+}); //end doc ready
