2ef220cd1db61967aa8fef3e82633a65dd0aa44a
Susannah Harris <harris.susannah@gmail.com>
2019-05-12 21:33:50 -0500

    finishing up, cannot figure out CSS and am getting frustrated

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -3,7 +3,7 @@ $( document ).ready( readyNow );
   let garage = [];
   //create empty Array
 
-  function newCar(){
+  function newCar( yearIn, makeIn, modelIn ){
     console.log( 'in newCar:' );
       let newCarObject = {
         year: $( '#yearIn' ).val(),
@@ -12,12 +12,31 @@ $( document ).ready( readyNow );
       }; //push cars into array
     garage.push(newCarObject);
     //empty inputs
-    $( '#yearIn' ).val( '' ),
-    $( '#makeIn' ).val( '' ),
+    $( '#yearIn' ).val( '' );
+    $( '#makeIn' ).val( '' );
     $( '#modelIn' ).val( '' );
-  } // end newCar
+    displayCars();
+    return true;
+    //update DOM
+  }//add cars into array
   //empty inputs
 
 function readyNow(){
+  console.log( 'JQ' );
   $( '#addNewCarButton' ).on( 'click', newCar );
+}
+//give button action
+
+function displayCars() {
+  console.log( 'in displayCars' );
+  //target output by ID
+  let el = $( '#carsGoIn'  );
+//empty
+  el.empty();
+//loop through purchases
+  for( car of garage ){
+    el.append( '<li>' + car.make + ', ' + car.model + ', ' + car.year + '</li>');
+  }
+//for each purchase, create a list
+//display cars in the garage
 }
