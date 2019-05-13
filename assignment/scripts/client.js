d10b4e98fab5d1dfcf3a24dd4e6c3c6fa66b0cad
Susannah Harris <harris.susannah@gmail.com>
2019-05-12 20:21:01 -0500

    edited buttons to include added input with one button

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -1,4 +1,41 @@
+$( document ).ready( readyNow );
+
+function readyNow(){
+  console.log( 'JQ' );
+
+function addNewCar(){
+  console.log( 'in addNewCar' );
+  //get user inputs
+  //create new object
+} //end addNewCar
+
+  $( '#addNewCarButton' ).on( 'click', addNewCar );
+}
+//end readyNow
+//handle click event
+
 let garage = [];
+//create empty Array
+
+function addNewCar(){
+  console.log( 'addNewCar pressed' );
+  let buttonOutput = 'Car added, ' +  $( '#makeInput').val();
+  console.log(buttonOutput);
+}
+
+function addNewCar(){
+  console.log( 'addNewCar pressed' );
+  let buttonOutput = $( '#modelInput').val();
+  console.log(buttonOutput);
+}
+
+function addNewCar(){
+  console.log( 'addNewCar pressed' );
+  let buttonOutput = $( '#yearInput').val();
+  console.log(buttonOutput);
+}
+//target car input elements
+
 
 function newCar( yearIn, makeIn, modelIn ){
   console.log( 'in newCar:', yearIn, makeIn, modelIn );
@@ -6,7 +43,7 @@ function newCar( yearIn, makeIn, modelIn ){
     year: yearIn,
     make: makeIn,
     model: modelIn
-  }
+  };
   garage.push(newCarObject);
   return true;
 } // end newCar
